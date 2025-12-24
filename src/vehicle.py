88637212dadbd907ccbd1f030eaecd1b157360c2
vilas <vilaskr2962@gmail.com>
2025-12-24 16:24:49 +0530

    Modify Program : [ Vilas ] Implemented Encapsulation

--- src/vehicle.py
+++ src/vehicle.py
@@ -3,4 +3,37 @@ class Vehicle:
     def __init__(self, id, model, battery_percentage):
         self.id = id
         self.model = model
-        self.battery_percentange = battery_percentage
+        self.battery_percentage = battery_percentage
+        self.__maintenance_status = None
+        self.__rental_price = None
+        
+    @property
+    def maintenance_status(self):
+        return self.__maintenance_status
+    
+    @maintenance_status.setter
+    def maintenance_status(self, maintenance_status):
+        self.__maintenance_status = maintenance_status
+        
+    @property
+    def rental_price(self):
+        return self.__rental_price
+    
+    @rental_price.setter
+    def rental_price(self, rental_price):
+        self.__rental_price = rental_price
+    
+    @property
+    def battery_percentage(self):
+        return self.__battery_percentage
+        
+    @battery_percentage.setter
+    def battery_percentage(self, battery_percentage):
+        if 0 <= battery_percentage <= 100:
+            self.__battery_percentage = battery_percentage 
+        else:    
+            self.__battery_percentage = None  
+            raise ValueError("Battery percentage should be between 0 and 100")
+        
+        
+
